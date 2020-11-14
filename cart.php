ca7167fea0e597c6727cc8f81ed7058c165833ae
Aurélie Fqt <70887548+AurelieFqt@users.noreply.github.com>
2020-11-14 16:22:53 +0100

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require 'inc/data/products.php'; 
+
+if (!isset($_SESSION['loginname'])){
+    header('Location: login.php');
+    die();
+}
+
+require 'inc/head.php'; ?>
+<section class="cookies container-fluid">
+    <div class="row">
+        <ul>
+            <?php 
+            foreach($_SESSION['cart'] as $id => $quantity){
+                echo '<li>' . $catalog[$id]['name'] . ' -> Quantity : ' . $quantity . '</li>';
+        }
+        ?>
+        </ul>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
